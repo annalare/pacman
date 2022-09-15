f705afcd615fabb3e6d2f9a8750e39a78898aad0
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-09-15 17:30:36 -0300

    feat : mouth

--- index.js
+++ index.js
@@ -24,18 +24,36 @@ class Player {
     this.position = position;
     this.velocity = velocity;
     this.radius = 15;
+    this.radians = 0.75;
+    this.openRate = 0.12;
+    this.rotation = 0;
   }
   draw() {
+    c.save();
+    c.translate(this.position.x, this.position.y);
+    c.rotate(this.rotation);
+    c.translate(-this.position.x, -this.position.y);
     c.beginPath();
-    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.arc(
+      this.position.x,
+      this.position.y,
+      this.radius,
+      this.radians,
+      Math.PI * 2 - this.radians
+    );
+    c.lineTo(this.position.x, this.position.y);
     c.fillStyle = "yellow";
     c.fill();
     c.closePath;
+    c.restore();
   }
   update() {
     this.draw();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
+
+    if (this.radians < 0 || this.radians > 0.75) this.openRate = -this.openRate;
+    this.radians += this.openRate;
   }
 }
 class Ghost {
@@ -507,6 +525,12 @@ function animate() {
       }
     }
   }
+
+  //win condition
+  if (pellets.length === 0) {
+    cancelAnimationFrame(animationId);
+  }
+
   for (let i = pellets.length - 1; 0 <= i; i--) {
     const pellet = pellets[i];
     pellet.draw();
@@ -639,7 +663,12 @@ function animate() {
       ghost.prevCollisions = [];
     }
   });
+  if (player.velocity.x > 0) player.rotation = 0;
+  else if (player.velocity.x < 0) player.rotation = Math.PI;
+  else if (player.velocity.y > 0) player.rotation = Math.PI / 2;
+  else if (player.velocity.y < 0) player.rotation = Math.PI * 1.5;
 }
+
 animate();
 
 addEventListener("keydown", ({ key }) => {
